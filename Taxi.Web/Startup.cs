917fa0acfd86217ab5ac18e7209f58823589ff39
JOEL ABDIEL ADRIAN MACHICADO <jadrianm@est.emi.edu.bo>
2020-02-21 00:39:54 -0400

    ApiTaxicontroller

--- Taxi.Web/Startup.cs
+++ Taxi.Web/Startup.cs
@@ -54,6 +54,7 @@ namespace Taxi.Web
 
             services.AddTransient<SeedDb>();
             services.AddScoped<IUserHelper, UserHelper>();
+            services.AddScoped<IConverterHelper, ConverterHelper>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
