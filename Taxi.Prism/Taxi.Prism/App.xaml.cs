a71071347fa9507b0dac184594b21d6d8830782f
JOEL ABDIEL ADRIAN MACHICADO <jadrianm@est.emi.edu.bo>
2020-02-21 02:33:16 -0400

    NugetInstaladosMap

--- Taxi.Prism/Taxi.Prism/App.xaml.cs
+++ Taxi.Prism/Taxi.Prism/App.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using Prism;
 using Prism.Ioc;
-using Taxi.Prism.ViewModels;
-using Taxi.Prism.Views;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,11 +9,7 @@ namespace Taxi.Prism
 {
     public partial class App
     {
-        /* 
-         * The Xamarin Forms XAML Previewer in Visual Studio uses System.Activator.CreateInstance.
-         * This imposes a limitation in which the App class must have a default constructor. 
-         * App(IPlatformInitializer initializer = null) cannot be handled by the Activator.
-         */
+        
         public App() : this(null) { }
 
         public App(IPlatformInitializer initializer) : base(initializer) { }
@@ -29,7 +24,7 @@ namespace Taxi.Prism
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
             containerRegistry.RegisterForNavigation<NavigationPage>();
-            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+           
         }
     }
 }
