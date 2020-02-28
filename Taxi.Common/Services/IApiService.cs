b9c1c1ff9b9bf3b5ce91285b498bdf44e573f31f
JOEL ABDIEL ADRIAN MACHICADO <jadrianm@est.emi.edu.bo>
2020-02-28 02:08:07 -0400

    ConsultaPlacaapp

---
+++ Taxi.Common/Services/IApiService.cs
@@ -0,0 +1,11 @@
+﻿using Taxi.Common.Models;
+using System.Threading.Tasks;
+
+namespace Taxi.Common.Services
+{
+    public interface IApiService
+    {
+        Task<Response> GetTaxiAsync(string plaque, string urlBase, string servicePrefix, string controller);
+    }
+}
+
